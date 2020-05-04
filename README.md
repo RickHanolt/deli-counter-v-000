b8269ee86f6031badc424b2a539cecd960b64ebd
Richard Hanolt <rhanolt@gmail.com>
2020-05-04 18:25:54 +0000

    Automatically backed up by Learn

--- README.md
+++ README.md
@@ -49,3 +49,37 @@ Example usage:
 
 
 <p data-visibility='hidden'>View <a href='https://learn.co/lessons/deli-counter' title='Deli Counter - Take a Number'>Deli Counter - Take a Number</a> on Learn.co and start learning to code for free.</p>
+
+def now_serving(array)
+  if array.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array.first}."
+    array.shift
+  end
+end
+
+def take_a_number(array, customer)
+    array.push(customer)
+    puts "Welcome, #{customer}. You are number #{array.size} in line."
+end
+
+def line(array)
+  if array.size == 0
+     puts "The line is currently empty."
+  else
+    new_array = []
+    array.each_with_index { |array, index|
+      if index == 0
+        new_element = (index+1).to_s + ". "
+        new_array.push(new_element)
+        new_array.push(array)
+      else
+        new_element = " " + (index+1).to_s + ". "
+        new_array.push(new_element)
+        new_array.push(array)
+      end
+    }
+    puts "The line is currently: #{new_array.join}"
+  end
+end
--- deli_counter.rb
+++ deli_counter.rb
@@ -1,34 +1,20 @@
 # Write your code here.
-def now_serving(array)
-  if array.size == 0
-    puts "There is nobody waiting to be served!"
+def line(katz_deli)
+  if katz_deli == []
+    puts "The line is currently empty."
   else
-    puts "Currently serving #{array.first}."
-    array.shift
+    puts "The line is currently: #{katz_deli.join(" a ")}"
   end
 end
 
-def take_a_number(array, customer)
-    array.push(customer)
-    puts "Welcome, #{customer}. You are number #{array.size} in line."
+def take_a_number(katz_deli)
+
 end
 
-def line(array)
-  if array.size == 0
-     puts "The line is currently empty."
-  else
-    new_array = []
-    array.each_with_index { |array, index|
-      if index == 0
-        new_element = (index+1).to_s + ". "
-        new_array.push(new_element)
-        new_array.push(array)
-      else
-        new_element = " " + (index+1).to_s + ". "
-        new_array.push(new_element)
-        new_array.push(array)
-      end
-    }
-    puts "The line is currently: #{new_array.join}"
-  end
+def now_serving(katz_deli)
+
+end
+
+def take_a_number(katz_deli, new_customer)
+
 end
